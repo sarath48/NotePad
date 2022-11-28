3b8c90baee3637a46b9a926dbecb06b4e265bd50
Bhuvanagiri <2166191@cognizant.com>
2022-11-28 11:45:51 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'notepad';
+  notesArray: string[] = [];
+  inputNotes: string = '';
+
+  updateNotesArray() {
+    if (this.inputNotes !== '') {
+      this.notesArray.push(this.inputNotes);
+      this.inputNotes = '';
+    }
+  }
+
+  deleteInputNotes() {
+    this.inputNotes = '';
+  }
 }
